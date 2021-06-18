b188dd615ef1783b8555c1f8e7c04cc10a84eba2
Enoch Hobenu <enochhobenu@yahoo.com>
2021-06-18 21:23:25 +0000

    finally

--- New_Project.R
+++ New_Project.R
@@ -2,3 +2,6 @@ print('this is an existing project')
 
 print('testing whether this would appear in github')
 Print('i dont get it why the push button is grayed out')
+print('i cant believe i finally figured it out using the shell in RStudio
+      instead of git bash helping me out')
+print('awww boys abre ankasa, who say man no dey?')
